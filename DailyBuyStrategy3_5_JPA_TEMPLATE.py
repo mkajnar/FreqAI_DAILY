cbf3f63facb34634622aab07af3295698054051d
root <root@AORUS.localdomain>
2026-02-08 22:31:15 +0100

    Add all indicators as hyperopt parameters

--- DailyBuyStrategy3_5_JPA_TEMPLATE.py
+++ DailyBuyStrategy3_5_JPA_TEMPLATE.py
@@ -38,23 +38,46 @@ class {{CLASS_NAME}}(IStrategy):
     trailing_stop_positive = 0.003
     trailing_stop_positive_offset = 0.008
 
-    # Hyperoptable parameters
+    # RSI parameters
+    rsi_timeperiod = IntParameter(5, 50, default=14, space='buy', optimize=True)
+
+    # MACD parameters
+    macd_fast = IntParameter(5, 30, default=12, space='buy', optimize=True)
+    macd_slow = IntParameter(15, 50, default=26, space='buy', optimize=True)
+    macd_signal = IntParameter(3, 20, default=9, space='buy', optimize=True)
+
+    # Bollinger Bands parameters
+    bb_window = IntParameter(10, 50, default=20, space='buy', optimize=True)
+    bb_stds = DecimalParameter(1.0, 4.0, default=2.0, space='buy', optimize=True)
+
+    # EMA parameters (buy)
     buy_ema_short = IntParameter(5, 50, default=10, space='buy', optimize=True)
     buy_ema_long = IntParameter(50, 200, default=50, space='buy', optimize=True)
+
+    # EMA parameters (sell)
     sell_ema_short = IntParameter(5, 50, default=10, space='sell', optimize=True)
     sell_ema_long = IntParameter(50, 200, default=50, space='sell', optimize=True)
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=14)
-        macd = ta.MACD(dataframe)
+        # RSI
+        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=self.rsi_timeperiod.value)
+
+        # MACD
+        macd = ta.MACD(dataframe, fastperiod=self.macd_fast.value, slowperiod=self.macd_slow.value, signalperiod=self.macd_signal.value)
         dataframe['macd'] = macd['macd']
         dataframe['macdsignal'] = macd['macdsignal']
+        dataframe['macdhist'] = macd['macdhist']
+
+        # EMA (buy)
         dataframe['ema_short'] = ta.EMA(dataframe, timeperiod=self.buy_ema_short.value)
         dataframe['ema_long'] = ta.EMA(dataframe, timeperiod=self.buy_ema_long.value)
+
+        # EMA (sell)
         dataframe['sell_ema_short'] = ta.EMA(dataframe, timeperiod=self.sell_ema_short.value)
         dataframe['sell_ema_long'] = ta.EMA(dataframe, timeperiod=self.sell_ema_long.value)
 
-        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        # Bollinger Bands
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=self.bb_window.value, stds=float(self.bb_stds.value))
         dataframe['bb_lowerband'] = bollinger['lower']
         dataframe['bb_middleband'] = bollinger['mid']
         dataframe['bb_upperband'] = bollinger['upper']
@@ -66,7 +89,8 @@ class {{CLASS_NAME}}(IStrategy):
             (
                 (dataframe['volume'] > 0) &
                 (dataframe['ema_short'] > dataframe['ema_long']) &
-                (dataframe['macd'] > dataframe['macdsignal'])
+                (dataframe['macd'] > dataframe['macdsignal']) &
+                (dataframe['rsi'] > 50)
             ),
             ['enter_long', 'enter_tag']
         ] = (1, 'ema_macd_buy')
@@ -78,7 +102,8 @@ class {{CLASS_NAME}}(IStrategy):
             (
                 (dataframe['volume'] > 0) &
                 (dataframe['sell_ema_short'] < dataframe['sell_ema_long']) &
-                (dataframe['macd'] < dataframe['macdsignal'])
+                (dataframe['macd'] < dataframe['macdsignal']) &
+                (dataframe['rsi'] < 50)
             ),
             ['exit_long', 'exit_tag']
         ] = (1, 'ema_macd_sell')
